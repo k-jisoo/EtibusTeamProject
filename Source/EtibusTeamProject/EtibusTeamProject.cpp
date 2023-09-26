9d841492dfa1a05aa3373c74775ed2b4379e528b
k-jisoo <boy7442@gmail.com>
2023-09-26 16:13:38 +0900

    C++ 프로젝트로 변경

---
+++ Source/EtibusTeamProject/EtibusTeamProject.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EtibusTeamProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EtibusTeamProject, "EtibusTeamProject" );
