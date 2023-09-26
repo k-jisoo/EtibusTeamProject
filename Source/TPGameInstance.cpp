d573fb7791aaf06278ea8faca7e3eadb6f98cd16
k-jisoo <boy7442@gmail.com>
2023-09-26 16:14:43 +0900

    캐릭터, 애니메이션, Entry 위젯, Lobby 위젯 코드 추가

---
+++ Source/TPGameInstance.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TPGameInstance.h"
+#include "BaseCharacter.h"
+#include "Engine/DataTable.h"
+
+void UTPGameInstance::ReqGetCharacterRowData_Implementation(FName name)
+{
+	GetCharacterRowData(name);
+}
+
+void UTPGameInstance::GetCharacterRowData(FName name)
+{
+	if (!CharacterData)
+		return;
+
+	MyCharacter = CharacterData->FindRow<FST_Character>(name, TEXT("")) ? CharacterData->FindRow<FST_Character>(name, TEXT("")) : nullptr;
+}
---
+++ Source/TPGameInstance.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "TPGameInstance.generated.h"
+
+struct FST_Character;
+/**
+ * 
+ */
+UCLASS()
+class ETIBUSTEAMPROJECT_API UTPGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Data")
+	FString Username;
+
+	FST_Character* MyCharacter;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class UDataTable* CharacterData;
+
+	UFUNCTION(Server, Reliable)
+	void ReqGetCharacterRowData(FName name);
+
+	void GetCharacterRowData(FName name);
+
+};
