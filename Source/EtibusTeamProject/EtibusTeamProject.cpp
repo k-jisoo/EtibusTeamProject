8ca3da36b0ed9109a35c4611096dfb3a89314fc8
k-jisoo <boy7442@gmail.com>
2023-09-27 12:43:34 +0900

    Plugins, Source 삭제

--- Source/EtibusTeamProject/EtibusTeamProject.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "EtibusTeamProject.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EtibusTeamProject, "EtibusTeamProject" );
